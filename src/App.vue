f02751e2a5ad4ab2f2c38c7879c1c72fca13f248
Smail <44433524+Smail@users.noreply.github.com>
2022-06-13 15:47:33 +0200

    feat: style page

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,35 @@
 </template>
 
 <style>
+/* TODO GDPR - download font locally */
+@import url("https://fonts.googleapis.com/css2?family=Poppins&display=swap");
+
+:root {
+  /* https://developer.apple.com/design/human-interface-guidelines/foundations/color/#specifications */
+  --apple-gray-6-dark: rgb(28, 28, 30);
+  --apple-blue-dark: rgb(10, 132, 255);
+  --apple-red-dark: rgb(255, 69, 58);
+  --apple-pink-dark: rgb(255, 69, 58);
+  --font-color: white;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  min-width: 100vw;
+  min-height: 100vh;
+  color: var(--font-color);
+  background-color: var(--apple-gray-6-dark);
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+
+  font-family: Poppins, Helvetica, Arial, sans-serif;
   text-align: center;
-  color: #2c3e50;
+  display: flex;
+  flex-direction: column;
 }
 
 nav {
@@ -21,7 +44,7 @@ nav {
 
 nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: var(--apple-red-dark);
 }
 
 nav a.router-link-exact-active {
