4aae159c6b39e886d0f4727f160f63403ea7fcfc
Smail <44433524+Smail@users.noreply.github.com>
2022-06-13 23:23:46 +0200

    test(mock): add mock cards

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,22 @@ export default createStore({
     firstName: "Max",
     lastName: "Mustermann",
     decks: [
-      { id: 1, name: "Deck 1" },
+      {
+        id: 1,
+        name: "Deck 1",
+        cards: [
+          {
+            id: 1,
+            front: "This is the front page of a flash card 1",
+            back: "This is the back page of a flash card 1",
+          },
+          {
+            id: 2,
+            front: "This is the front page of a flash card 2",
+            back: "This is the back page of a flash card 2",
+          },
+        ],
+      },
       { id: 2, name: "Deck 2" },
       { id: 3, name: "Deck 3" },
     ],
