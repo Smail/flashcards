3248959a3e0788908cc24f35090e925c12b1c55f
Smail <44433524+Smail@users.noreply.github.com>
2022-06-13 17:38:14 +0200

    feat: add headers in cards

--- src/views/CardsView.vue
+++ src/views/CardsView.vue
@@ -1,10 +1,38 @@
 <template>
-  <div class="cards"></div>
+  <div class="cards">
+    <h1>Cards</h1>
+    <!-- Display current deck name -->
+    <h6 id="current-deck-header">{{ this.currentDeck }}</h6>
+    <div></div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "CardsView",
   components: {},
+  computed: {
+    currentDeck() {
+      const currentDeckId = this.$store.state.currentDeckId;
+      const currentDeck = this.$store.state.decks.find(
+        (deck) => deck.id === currentDeckId
+      );
+
+      return currentDeck;
+    },
+  },
 };
 </script>
+
+<style>
+.cards {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+#current-deck-header {
+  font-weight: normal;
+  font-style: italic;
+}
+</style>
