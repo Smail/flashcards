c4eed338f1d64d39adbf28cee172625fc566a28e
Smail <44433524+Smail@users.noreply.github.com>
2022-06-13 16:52:22 +0200

    feat: show name of user and use component class

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="home"></div>
+  <div class="home component">
+    <h1>{{ this.$store.state.firstName }} {{ this.$store.state.lastName }}</h1>
+    <div class="statistic"></div>
+  </div>
 </template>
 
 <script>
@@ -8,3 +11,19 @@ export default {
   components: {},
 };
 </script>
+
+<style scoped>
+.home {
+  background-color: var(--apple-gray-5-dark);
+  display: flex;
+  flex-direction: column;
+}
+
+.statistic {
+  background-color: var(--apple-gray-5-dark);
+}
+
+h1 {
+  margin: 0;
+}
+</style>
