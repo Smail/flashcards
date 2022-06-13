962a058125aafce42223509d6f705223a7603bd4
Smail <44433524+Smail@users.noreply.github.com>
2022-06-13 19:55:38 +0200

    test: add mock deck values

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,12 @@ export default createStore({
   state: {
     firstName: "Max",
     lastName: "Mustermann",
-    decks: [],
-    currentDeckId: undefined,
+    decks: [
+      { id: 1, name: "Deck 1" },
+      { id: 2, name: "Deck 2" },
+      { id: 3, name: "Deck 3" },
+    ],
+    currentDeckId: 1,
   },
   getters: {},
   mutations: {},
