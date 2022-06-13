754375fc30036aa5108bde924205a9f315ad93b6
Smail <44433524+Smail@users.noreply.github.com>
2022-06-13 21:21:38 +0200

    feat: add quick selection segment in HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,48 @@
 <template>
   <h1>{{ this.$store.state.firstName }} {{ this.$store.state.lastName }}</h1>
-  <div class="statistic"></div>
+  <ul id="quick-selection">
+    <li v-for="item in quickSelectionButtons" class="quick-select-btn">
+      <router-link
+        style="flex: 1"
+        class="component translate-all-03s hover-tranform-animation"
+        :to="item.href"
+      >
+        <h2>{{ item.title }}</h2>
+      </router-link>
+    </li>
+  </ul>
 </template>
 
 <script>
 export default {
   name: "HomeView",
   components: {},
+  data() {
+    return {
+      quickSelectionButtons: [
+        { title: "Study", href: "/study" },
+        { title: "Browse", href: "/browse" },
+        { title: "Add", href: "" },
+        { title: "Edit", href: "" },
+        { title: "Statistics", href: "" },
+      ],
+    };
+  },
 };
 </script>
+
+<style>
+#quick-selection {
+  align-self: stretch;
+  justify-content: stretch;
+  align-items: stretch;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+}
+
+.quick-select-btn {
+  display: flex;
+  justify-content: center;
+  align-items: stretch;
+}
+</style>
