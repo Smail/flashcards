9a09a0310ce4169ebfddc4e97489cd20de26334f
Smail <44433524+Smail@users.noreply.github.com>
2022-06-13 16:18:04 +0200

    feat: add DecksView

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/StudyView.vue"),
+      import(/* webpackChunkName: "study" */ "../views/StudyView.vue"),
+  },
+  {
+    path: "/decks",
+    name: "decks",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "decks" */ "../views/DecksView.vue"),
   },
   {
     path: "/about",
---
+++ src/views/DecksView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="decks"></div>
+</template>
+
+<script>
+export default {
+  name: "DecksView",
+  components: {},
+};
+</script>
