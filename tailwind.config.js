d84e1213f04ef88fc43dda5aaa45d36fa587d84f
Smail <44433524+Smail@users.noreply.github.com>
2022-06-29 21:26:44 +0200

    feat: add themes to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,37 @@
+const defaultColors = require("tailwindcss/colors");
+
+const generateColorClass = (variable) => {
+  return ({ opacityValue }) =>
+    opacityValue
+      ? `rgba(var(--${variable}), ${opacityValue})`
+      : `rgb(var(--${variable}))`;
+};
+
+const textColor = {
+  primary: generateColorClass("text-primary"),
+  secondary: generateColorClass("text-secondary"),
+  tertiary: generateColorClass("text-tertiary"),
+};
+
+const backgroundColor = {
+  primary: generateColorClass("bg-primary"),
+  secondary: generateColorClass("bg-secondary"),
+  tertiary: generateColorClass("bg-tertiary"),
+};
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    colors: defaultColors,
+    fontFamily: {
+      sans: ["Poppins", "Helvetica", "Arial", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      textColor,
+      backgroundColor,
+    },
   },
   plugins: [],
 };
