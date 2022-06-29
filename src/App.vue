c6a98f203b3023b4983b9f98a3f4f8cab04c6786
Smail <44433524+Smail@users.noreply.github.com>
2022-06-29 22:04:37 +0200

    refactor(tailwind): remove themes from css (use tailwind config instead)

--- src/App.vue
+++ src/App.vue
@@ -26,59 +26,9 @@ export default {
 </script>
 
 <style>
-:root {
-  /* https://developer.apple.com/design/human-interface-guidelines/foundations/color/#specifications */
-  --apple-gray-4-dark: rgb(58, 58, 60);
-  --apple-gray-5-dark: rgb(44, 44, 46);
-  --apple-gray-6-dark: rgb(28, 28, 30);
-  --apple-red-dark: rgb(255, 69, 58);
-  --apple-orange-dark: rgb(255, 159, 10);
-  --apple-green-dark: rgb(48, 209, 88);
-  --apple-blue-dark: rgb(10, 132, 255);
-  --apple-pink-dark: rgb(255, 55, 95);
-}
-
-@media (prefers-color-scheme: light) {
-  :root {
-    --font-color: black;
-
-    --primary-color: var(--apple-blue-dark);
-    --secondary-color: var(--apple-green-dark);
-    --background-color: white;
-  }
-
-  .component {
-    background-color: rgba(0, 0, 0, 0.15);
-  }
-}
-
-@media (prefers-color-scheme: dark) {
-  :root {
-    --font-color: white;
-
-    --primary-color: var(--apple-pink-dark);
-    --secondary-color: var(--apple-orange-dark);
-    --background-color: var(--apple-gray-6-dark);
-  }
-
-  .component {
-    background-color: rgba(255, 255, 255, 0.07);
-  }
-}
-
-::selection {
-  background-color: var(--primary-color);
-}
-
-body {
-  margin: 0;
-  padding: 0;
-  min-width: 100vw;
-  min-height: 100vh;
-  color: var(--font-color);
-  background-color: var(--background-color);
-}
-
+:root,
+html,
+body,
 #app {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
